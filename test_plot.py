125db1e264f4322b85090d842ae5f2b703b2c2d2
peilin.gatech <peilin.gatech@gmail.com>
2015-01-14 00:11:27 -0500

    new test_plot.py file

---
+++ test_plot.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  8 18:09:05 2015
+
+@author: applepei
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as lines
+
+class HighlightSelected(lines.VertexSelector):
+    def __init__(self, line, fmt='ro', **kwargs):
+        lines.VertexSelector.__init__(self, line)
+        self.markers, = self.axes.plot([], [], fmt, **kwargs)
+
+    def process_selected(self, ind, xs, ys):
+        self.markers.set_data(xs, ys)
+        self.canvas.draw()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+x, y = np.random.rand(2, 30)
+line, = ax.plot(x, y, 'bs-', picker=5)
+
+selector = HighlightSelected(line)
+plt.show()
